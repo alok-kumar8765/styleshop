108b24e614ede5e45b4ef24ca895529e9c38fb6c
alok8765 <85283226+alok-techqware@users.noreply.github.com>
2021-08-26 17:47:11 +0530

    first commit

--- StyleShop/settings.py
+++ StyleShop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#l03^#l0f4!d5+0_x!5jvxc#)@rm*9c(2$-ux*z1btf$+))i@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['12.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
